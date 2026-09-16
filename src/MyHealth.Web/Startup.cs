501bcd67c550c98126afbcb73a5604d5ca82e679
nbardoloi <nbardoloi@gmail.com>
2026-09-16 16:35:56 +0530

    Update Startup.cs

--- src/MyHealth.Web/Startup.cs
+++ src/MyHealth.Web/Startup.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using MyHealth.Web.AppBuilderExtensions;
 using MyHealth.Model;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using MyHealth.Data;
 using MyHealth.API.Infrastructure;
 using Microsoft.Extensions.Configuration;
@@ -16,7 +14,7 @@ namespace MyHealth.Web
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IWebHostEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -25,8 +23,7 @@ namespace MyHealth.Web
 
             if (env.IsDevelopment())
             {
-                builder.AddUserSecrets("");
-                builder.AddApplicationInsightsSettings(developerMode: true);
+                builder.AddUserSecrets<Startup>();
             }
 
             builder.AddEnvironmentVariables();
@@ -35,25 +32,20 @@ namespace MyHealth.Web
 
         public IConfiguration Configuration { get; set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             var connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<MyHealthContext>(options => options.UseSqlServer(connection));
 
-            // Register dependencies
             services.ConfigureDependencies();
 
-            // Add Identity services to the services container.
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<MyHealthContext>()
                 .AddDefaultTokenProviders();
 
             services.AddApplicationInsightsTelemetry(Configuration);
 
-            // Add MVC services to the services container.
-            services.AddMvc();
+            services.AddControllersWithViews();
 
             services.AddMemoryCache();
 
@@ -62,19 +54,11 @@ namespace MyHealth.Web
             services.AddAuthorization(Policies.Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
             MyHealthDataInitializer dataInitializer)
         {
-            // Add Application Insights monitoring to the request pipeline as a very first middleware.
-            app.UseApplicationInsightsRequestTelemetry();
-
-            loggerFactory.AddConsole();
-
-            // Add the following to the request pipeline only in development environment.
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -82,21 +66,19 @@ namespace MyHealth.Web
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            // Add Application Insights exceptions handling to the request pipeline.
-            app.UseApplicationInsightsExceptionTelemetry();
-
-            // Add static files to the request pipeline.
             app.UseStaticFiles();
+
+            app.UseRouting();
 
             app.UseSession();
 
             app.ConfigureSecurity();
 
-            // Add MVC to the request pipeline.
+            app.UseAuthorization();
+
             app.ConfigureRoutes();
 
-            await dataInitializer.InitializeDatabaseAsync(app.ApplicationServices);
-
+            dataInitializer.InitializeDatabaseAsync(app.ApplicationServices).GetAwaiter().GetResult();
         }
     }
 }
