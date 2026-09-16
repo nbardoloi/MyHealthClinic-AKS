9c8c074042f77b9c1fdec73674346841de6097a4
nbardoloi <nbardoloi@gmail.com>
2026-09-16 16:49:56 +0530

    Update Startup.Security.cs

--- src/MyHealth.Web/AppBuilderExtensions/Startup.Security.cs
+++ src/MyHealth.Web/AppBuilderExtensions/Startup.Security.cs
@@ -1,15 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 
 namespace MyHealth.Web.AppBuilderExtensions
 {
-
     public static class SecurityExtensions
     {
         public static IApplicationBuilder ConfigureSecurity(this IApplicationBuilder app)
         {
-            // Add cookie-based authentication to the request pipeline.
-            return app.UseIdentity();
+            return app.UseAuthentication();
         }
-
     }
 }
